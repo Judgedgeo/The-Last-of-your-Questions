c18bad3c8de5aa2d2a3d99519875f8d8ce539b9c
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2022-12-29 11:26:16 -0600

    fixed timer

--- script.js
+++ script.js
@@ -1,48 +1,23 @@
-// var myQuestions = [
-// {
-//     question: "What State is Austin located in?",
-//     answers:{},
-//     correctAnswer:Texas
-// },
-// {
-//     question: "What city is UT located in?",
-//     answers:{},
-//     correctAnswer:Austin
-// },
 
-//     {
-//         question: "What is the mascot for UT?",
-//         answers:{},
-//         correctAnswer:Bevo
-// },
-
-
-// ]
-
-
-const startingMinutes = 1;
-
-let time = startingMinutes * 60;
-
-const countdownEL = document.getElementById('countdown');
-
-setInterval(updateCountdown, 1000);
-
-function updateCountdown() {
-const minutes = Math.floor(time / 60);
-let seconds=time % 60;
-
-
-
-seconds = seconds <10 ? '0' + seconds : seconds;
-
-countdownEL.innerHTML = `${minutes}:${seconds}`;
-time--;
-
+var timeoutHandle;
+function countdown(minutes) {
+    var seconds = 60;
+    var mins = minutes
+    function countdown() {
+    var counter = document.getElementById("timer2");
+    var current_minutes = mins-1
+    seconds--;
+    counter.innerHTML =
+    current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+    if( seconds > 0 ) {
+    timeoutHandle=setTimeout(countdown, 1000);
 }
-// setTimeout(TIME IS UP!);
-
-
-// // function endTime(){
-// timeH.innerHTML = 'TIMES UP!'
-// }
+    else {
+    if(mins > 1){
+    setTimeout(function () { countdown(mins - 1); }, 1000);
+}
+    document.getElementById("timer2").innerHTML = "TIME IS UP!"
+}
+}
+    countdown();
+}
