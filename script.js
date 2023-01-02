d28b8c31a7a8fba5c30b051b853319721cb87181
JudgedGeo <115055273+Judgedgeo@users.noreply.github.com>
2023-01-01 22:36:12 -0600

    worked on JS file

--- script.js
+++ script.js
@@ -1,151 +1,136 @@
+var timeoutHandle;
+function countdown(minutes) {
+    var seconds = 60;
+    var mins = minutes
+    function countdown() {
+    var counter = document.getElementById("timer2");
+    var current_minutes = mins-1
+    seconds--;
+    counter.innerHTML =
+    current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+    if( seconds > 0 ) {
+    timeoutHandle=setTimeout(countdown, 1000);
+}
+    else {
+    if(mins > 1){
+    setTimeout(function () { countdown(mins - 1); }, 1000);
+}
+    document.getElementById("timer2").innerHTML = "TIME IS UP!"
+}
+}
+    countdown();
+}
+
 const startButton = document.getElementById('start-btn')
-const questionContainerElement = document.getElementById
-('question-container')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById ('question-container')
+const questionElement = document.getElementById ('question')
+const answerButtonsElement = document.getElementById ('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
 
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+currentQuestionIndex++
+setNextQuestion()
+})
 
 function startGame() {
-console.log('Started')
-startButton.classList.add('hide')
-questionContainerElement.classList.remove('hide')
-setNextQuestion()
+    console.log('Started')
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
 }
 
-function setNextQuestion(){
-
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer() {
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+    button.dataset.correct = answer.correct
+}
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+})
+}
 
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild  (answerButtonsElement.firstChild)
+}
 }
 
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+})
+    if (shuffledQuestions.length > currentQuestionIndex +1 ) {
+    nextButton.classList.remove('hide')
+} else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+}
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+    element.classList.add('correct')
+} else {
+    element.classList.add('wrong')
+}
+}
+
+function clearStatusClass(element){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
 
 
+const questions = [
+{
+    question: 'What does || represent in JS?',
+    answers:[
+        {text: 'OR', correct: true },
+        {text: 'AND', correct: false },
+]
+},
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var timeoutHandle;
-// function countdown(minutes) {
-//     var seconds = 60;
-//     var mins = minutes
-//     function countdown() {
-//     var counter = document.getElementById("timer2");
-//     var current_minutes = mins-1
-//     seconds--;
-//     counter.innerHTML =
-//     current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-//     if( seconds > 0 ) {
-//     timeoutHandle=setTimeout(countdown, 1000);
-// }
-//     else {
-//     if(mins > 1){
-//     setTimeout(function () { countdown(mins - 1); }, 1000);
-// }
-//     document.getElementById("timer2").innerHTML = "TIME IS UP!"
-// }
-// }
-//     countdown();
-// }
-
-
-// function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-// 	function showQuestions(questions, quizContainer){
-
-//         var myQuestions = [
-//             {
-//                 question: "What does i represent in JS?",
-//                 answers: {
-//                     a: '',
-//                     b: ''
-//                     c: ''
-//                 },
-//                 correctAnswer: ''
-//             },
-//             {
-//                 question: "What does '?' represent in JS?",
-//                 answers: {
-//                     a: '',
-//                     b: '',
-//                     c: ''
-//                 },
-//                 correctAnswer: ''
-//             },
-//             {
-//                 question: "What does '===' represent in JS?",
-//                 answers: {
-//                     a: '',
-//                     b: '',
-//                     c: ''
-//                 },
-//                 correctAnswer: ''
-//             },
-//             {
-//                 question: "What is this '{}' called in JS?"
-//                     a: '',
-//                     b: '',
-//                     c: ''
-//                 },
-//                 correctAnswer: ''
-//             },
-//         ];
-// 	}
-
-// 	function showResults(questions, quizContainer, resultsContainer){
-// 		// code will go here
-// 	}
-
-// 	// show the questions
-// 	showQuestions(questions, quizContainer);
-
-// 	// when user clicks submit, show results
-// 	submitButton.onclick = function(){
-// 		showResults(questions, quizContainer, resultsContainer);
-// 	}
+{
+    question: 'What does ~ represent in JS?',
+    answers:[
+        {text: 'NOT', correct: true },
+        {text: 'RIGHT SHIFT', correct: false },
+]
+},
+{
+    question: 'What does === represent in JS?',
+    answers:[
+        {text: 'equal value and type', correct: true },
+        {text: 'not equal to', correct: false },
+]
+},
+{
+    question: 'What does == represent in JS?',
+    answers:[
+        {text: 'equal to', correct: true },
+        {text: 'equal value', correct: false },
+]
+},
+]
